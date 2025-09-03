d55b7b28c144ff5f4068766a1e61f854ce1c30c7
Richie Mouhouadi <rd.mouhouadi@gmail.com>
2025-09-03 11:44:16 +0200

    Update peauseng.py
    added the related questions

--- form_app/topics/peauseng.py
+++ form_app/topics/peauseng.py
@@ -2,14 +2,27 @@ import streamlit as st
 
 def render():
     answers = {}
-    answers["How would you rate the overall smart metering solution?"] = st.radio(
-        "How would you rate the overall smart metering solution?",
-        ["Excellent", "Good", "Average", "Poor"]
+    answers["How happy are you with PeauseNG"] = st.radio(
+        "How happy are you with PeauseNG?",
+        ["Very happy", "Happy", "Not happy", "Desappointed"]
     )
-    answers["What aspects of the solution do you value the most?"] = st.text_area(
-        "What aspects of the solution do you value the most?"
+
+    answers["How easy is it to use?] = st.radio(
+        "How easy is it to use?,
+        ["Very easy", "Easy", "Difficult", "Very difficult"]
     )
-    answers["Any suggestions for improvement in general?"] = st.text_area(
-        "Any suggestions for improvement in general?"
+
+    answers["How easy it to manage the fleet and users base?"] = st.radio(
+        "How easy is it to manage the fleet and users base?",
+        ["Very easy", "Easy", "Difficult", "Very difficult"]
+    )
+
+    answers["Do you have issues with the devices/smart phones?"] = st.radio(
+        "Do you have issues with the devices/smart phones?",
+        ["Yes", "No"]
+    )
+    
+    answers["What do you think could be improved in PeauseNG and smart phones?"] = st.text_area(
+        "What do you think could be improved in PeauseNG and smart phones?",
     )
     return answers
