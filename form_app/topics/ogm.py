11891ee8941623b72478c6969eb6b4e5a64ac93d
Richie Mouhouadi <rd.mouhouadi@gmail.com>
2025-09-03 14:14:01 +0200

    Update ogm.py
    add related questions

--- form_app/topics/ogm.py
+++ form_app/topics/ogm.py
@@ -2,14 +2,36 @@ import streamlit as st
 
 def render():
     answers = {}
-    answers["How would you rate the overall smart metering solution?"] = st.radio(
-        "How would you rate the overall smart metering solution?",
-        ["Excellent", "Good", "Average", "Poor"]
+    answers["How happy are you with OGM?"] = st.radio(
+        "How happy are you with OGM?",
+        ["Very happy", "happy", "Not happy", "Desappointed"]
     )
-    answers["What aspects of the solution do you value the most?"] = st.text_area(
-        "What aspects of the solution do you value the most?"
+    answers["Do you find it easy to create/manage new sites, concentrators and components?"] = st.text_area(
+        "Do you find it easy to create/manage new sites, concentrators and components?"
     )
-    answers["Any suggestions for improvement in general?"] = st.text_area(
-        "Any suggestions for improvement in general?"
+    answers["Do you find it easy to monitor the gateway fleet?"] = st.text_area(
+        "Do you find it easy to monitor the gateway fleet (diagnostics, traffic trends, alarms, etc.?"
+    )
+    answers["Do you find it easy to visualize and understand alarms?"] = st.radio(
+        "Do you find it easy to visualize and understand alarms?",
+        ["Yes", "No"]
+    )
+    answers["Any issues or feedback on the different screens and data presented?"] = st.text_area(
+        "Any issues or feedback on the differenr screens and data presented?"
+    )
+    answers["How happy are you with the system availability time?"] = st.radio(
+        "How happy are you with the system availability time?",
+        ["Very happy", "happy", "Not happy", "Desappointed"]
+    )
+    answers["How happy are you with the system performance?"] = st.radio(
+        "How happy are you with the system performance?",
+        ["Ver happy", "happy", "Not happy", "Desappointed"]
+    )
+    answers["How happy are you with the application release management?"] = st.radio(
+        "How happy are you with the application release management (planning and execution)?",
+        ["Very happy", "happy", "Not happy", "Desappointed"]
+    )
+    answers["What do you think that could be improved in OGM?"] = st.text_area(
+        "What do you think that could be improved in OGM?
     )
     return answers
