1c180fc66327233cd83de685e34112e0894b8299
Richie Mouhouadi <rd.mouhouadi@gmail.com>
2025-09-03 11:12:12 +0200

    Update general.py
    added questions

--- form_app/topics/general.py
+++ form_app/topics/general.py
@@ -2,13 +2,25 @@ import streamlit as st
 
 def render():
     answers = {}
+    answers["How happy are you with the SUEZ Smart Metering contract?"] = st.radio(
+        "How happy are you with the SUEZ Smart Metering contract?",
+        ["Excellent", "Good", "Average", "Poor"]
+    )
+
     answers["How would you rate the overall smart metering solution?"] = st.radio(
         "How would you rate the overall smart metering solution?",
         ["Excellent", "Good", "Average", "Poor"]
     )
+    
     answers["What aspects of the solution do you value the most?"] = st.text_area(
         "What aspects of the solution do you value the most?"
     )
+
+    answers["How strongly would you recommend SUEZ as a Smart Metering Partner?"] = st.radio(
+        "How strongly would you recommend SUEZ as a Smart Metering Partner?",
+        ["Very Strongly", "Strongly", "Neutral", "Poorly", "Not recommend"]
+    )
+    
     answers["Any suggestions for improvement in general?"] = st.text_area(
         "Any suggestions for improvement in general?"
     )
