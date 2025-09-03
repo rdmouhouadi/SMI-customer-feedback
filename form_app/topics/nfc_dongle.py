dd425863bf8926c0d6fface6c1f7fc4e1f491968
Richie Mouhouadi <rd.mouhouadi@gmail.com>
2025-09-03 11:52:25 +0200

    Update nfc_dongle.py
    added related questions

--- form_app/topics/nfc_dongle.py
+++ form_app/topics/nfc_dongle.py
@@ -2,14 +2,30 @@ import streamlit as st
 
 def render():
     answers = {}
-    answers["How would you rate the overall smart metering solution?"] = st.radio(
-        "How would you rate the overall smart metering solution?",
-        ["Excellent", "Good", "Average", "Poor"]
+    answers["How happy are you with the NFC dongle?"] = st.radio(
+        "How happy are you with the NFC dongle?",
+        ["Very happy", "happy", "Not happy", "Desappointed"]
     )
-    answers["What aspects of the solution do you value the most?"] = st.text_area(
-        "What aspects of the solution do you value the most?"
+
+    answers["Is it easy to use?"] = st.radio(
+        "Is it easy to use?",
+        ["Yes", "No"]
     )
-    answers["Any suggestions for improvement in general?"] = st.text_area(
-        "Any suggestions for improvement in general?"
+    
+    answers["How happy are you with the battery and its autonomy?"] = st.text_area(
+        "How happy are you with the battery and its autonomy?"
+    )
+
+    answers["Is the battery easy to recharge?"] = st.radio(
+        "Is the battery easy to recharge?",
+        ["Yes", "No"]
+    )
+
+    answers["Do you have any issue with the NFC dongles?"] = st.text_area(
+        "Do you have any issue with the NFC dongles?"
+    )
+    
+    answers["What do you think could be improved in the NFC dongles?"] = st.text_area(
+        "What do you think could be improved in the NFC dongles?"
     )
     return answers
