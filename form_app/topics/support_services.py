5952b151a246da984cda71c312059dc686a0dc01
Richie Mouhouadi <rd.mouhouadi@gmail.com>
2025-09-03 14:46:16 +0200

    Update support_services.py
    added related questions

--- form_app/topics/support_services.py
+++ form_app/topics/support_services.py
@@ -2,14 +2,19 @@ import streamlit as st
 
 def render():
     answers = {}
-    answers["How would you rate the overall smart metering solution?"] = st.radio(
-        "How would you rate the overall smart metering solution?",
-        ["Excellent", "Good", "Average", "Poor"]
+    answers["How happy are you with our support services in general?"] = st.radio(
+        "How happy are you with our support services in general?",
+       ["Very happy", "Happy", "Not happy", "Desappointed"]
     )
-    answers["What aspects of the solution do you value the most?"] = st.text_area(
-        "What aspects of the solution do you value the most?"
+    answers["How happy are you with IT4US as a ticket management tool?"] = st.radio(
+        "how happy are you with IT4US as a ticket management tool?",
+        ["Very happy", "Happy", "Not happy", "Desappointed"]
     )
-    answers["Any suggestions for improvement in general?"] = st.text_area(
+    answers["How happy are you with our Project Management?"] = st.radio(
+        "How happy are you with our Project Management?",
+        ["Very happy", "Happy", "Not happy", "Desappointed"]
+    )
+    anwsers["Any suggestions for improvement?"] = st.text_area(
         "Any suggestions for improvement in general?"
     )
     return answers
